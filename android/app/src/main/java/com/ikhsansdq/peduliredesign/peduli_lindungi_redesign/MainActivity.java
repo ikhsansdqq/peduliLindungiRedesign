aa893c41a2ad04f77799f52252fead3131969cae
mhikmal03 <hikmalkp@gmail.com>
2022-12-17 22:15:17 +0700

    tes

---
+++ android/app/src/main/java/com/ikhsansdq/peduliredesign/peduli_lindungi_redesign/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ikhsansdq.peduliredesign.peduli_lindungi_redesign;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const PeduliLindungiApp());
+}
+
+class PeduliLindungiApp extends StatelessWidget {
+  const PeduliLindungiApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Peduli Lindungi Redesign',
+      debugShowCheckedModeBanner: false,
+      home: HomePagePDL(),
+    );
+  }
+}
+
+class HomePagePDL extends StatelessWidget {
+  const HomePagePDL({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double widthMedia = MediaQuery.of(context).size.width;
+    double heightMedia = MediaQuery.of(context).size.height;
+    double statusBar = MediaQuery.of(context).viewPadding.top;
+    String? commit;
+    return Scaffold(
+      backgroundColor: const Color(0xFF1D99D8),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: statusBar),
+            width: widthMedia,
+            height: heightMedia,
+            color: const Color(0xFF1D99D8),
+            child: Column(
+              children: [
+                Image.asset('assets/img/pdl-logo.png')
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
